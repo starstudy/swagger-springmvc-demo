ef091dd058c4b8b893e0c9f8a8df4f616c1d33cb
sxx <commitment@mogoroom.com>
2017-08-19 09:54:30 +0800

    v1.1 - divide group

---
+++ swaggerdemoT2/src/main/java/com/demo/swagger/controller/moduleb/SwaggerControllerB.java
@@ -0,0 +1,30 @@
+package com.demo.swagger.controller.moduleb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
+/**
+ * @author sxx
+ * @brief Demo controller
+ * @details
+ * @date 2017-08-12 15:10
+ */
+@Controller
+@RequestMapping("/swaggerdemob")
+@Api(tags = "A Demo B")
+public class SwaggerControllerB {
+
+    @ResponseBody
+    @RequestMapping(value = "/helloworld", method = RequestMethod.GET)
+    @ApiOperation(nickname = "swagger-helloworld", value = "Swagger的世界", notes = "测试HelloWorld")
+    public String helloWorld(@ApiParam(value = "昵称") @RequestParam String nickname) {
+        return "Hello world, " + nickname;
+    }
+}
